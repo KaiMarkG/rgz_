5c535d3e112edada07ae49a8fe49970aa34ed386
KaiMarkG <121171822+KaiMarkG@users.noreply.github.com>
2022-12-21 19:52:03 +0700

    Update ModelStructures.cpp

--- rgz/ModelStructures.cpp
+++ rgz/ModelStructures.cpp
@@ -8,21 +8,13 @@ float dist(float x1, float y1, float x2, float y2) {
 
 bool linePoint(float x1, float y1, float x2, float y2, float px, float py) {
 
-	// get distance from the point to the two ends of the line
 	float d1 = dist(px, py, x1, y1);
 	float d2 = dist(px, py, x2, y2);
 
-	// get the length of the line
 	float lineLen = dist(x1, y1, x2, y2);
 
-	// since floats are so minutely accurate, add
-	// a little buffer zone that will give collision
-	float buffer = 0.05;    // higher # = less accurate
+	float buffer = 0.05;   
 
-	// if the two distances are equal to the line's
-	// length, the point is on the line!
-	// note we use the buffer here to give a range,
-	// rather than one #
 	if (d1 + d2 >= lineLen - buffer && d1 + d2 <= lineLen + buffer) {
 		return true;
 	}
@@ -49,31 +41,22 @@ bool CircleObst::coll_point(float px, float py, float rr) {
 }
 
 bool CircleObst::coll_line(float x1, float y1, float x2, float y2, float rr) {
-	// is either end INSIDE the circle?
-	// if so, return true immediately
 	bool inside1 = coll_point(x1, y1, rr);
 	bool inside2 = coll_point(x2, y2, rr);
 	if (inside1 || inside2) return true;
 
-	// get length of the line
 	float distX = x1 - x2;
 	float distY = y1 - y2;
 	float len = sqrt((distX * distX) + (distY * distY));
 
-	// get dot product of the line and circle
 	float dot = (((x - x1) * (x2 - x1)) + ((y - y1) * (y2 - y1))) / pow(len, 2);
 
-	// find the closest point on the line
 	float closestX = x1 + (dot * (x2 - x1));
 	float closestY = y1 + (dot * (y2 - y1));
 
-	// is this point actually on the line segment?
-	// if so keep going, but if not, return false
 	bool onSegment = linePoint(x1, y1, x2, y2, closestX, closestY);
 	if (!onSegment) return false;
 
-
-	// get distance to closest point
 	distX = closestX - x;
 	distY = closestY - y;
 	float distance = sqrt((distX * distX) + (distY * distY));
